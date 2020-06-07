00e2c1e28da72863bcc1bcce952d9ffe6e27ac5f
Raphael Gozlan <raphaelior09@gmail.com>
2020-06-07 18:37:18 +0300

    event

--- notifier.py
+++ notifier.py
@@ -2,16 +2,22 @@ import sys
 import time
 import logging
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import *
 
-if __name__ == "__main__":
+
+def on_modified(self,  event):
+    print(event.event_type)
+
+
+def main():
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
+    event_handler = PatternMatchingEventHandler(patterns='*.txt', ignore_directories=True)
+    FileSystemEventHandler.on_modified = on_modified
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, path, recursive=False)
     observer.start()
     try:
         while True:
@@ -19,3 +25,7 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
+
+
+if __name__ == "__main__":
+    main()
